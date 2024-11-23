49fc4ce45ff3cd38e4577316e9ec2ee5118fbd44
sm-karthi <karthikeyan24022007@gmail.com>
2024-11-23 14:32:52 +0530

    sign up email check

--- assets/pages/scripts/login.js
+++ assets/pages/scripts/login.js
@@ -99,11 +99,9 @@ function validatePassword(password) {
 
 // Validate email format
 function validateEmail(email) {
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?!.*\1.*\1)$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z][a-zA-Z0-9.-]*\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email);
 }
-
-
 
 signUpForm.addEventListener("submit", async (e) => {
     e.preventDefault(); // Prevent form submission initially
